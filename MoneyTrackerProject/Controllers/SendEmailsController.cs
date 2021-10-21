c8423872e12d8794b6e8a86c86e60490345bff6a
ben-vt <61205390+ben-vt@users.noreply.github.com>
2021-10-21 15:14:34 +0530

    Added option to send Bulk Email -Separated based on department of accountant -Admin and manager can send to all employees -Accountants can send bulk email to employees within their department

--- MoneyTrackerProject/Controllers/SendEmailsController.cs
+++ MoneyTrackerProject/Controllers/SendEmailsController.cs
@@ -1,16 +1,21 @@
 ﻿using MoneyTrackerProject.Utilities;
 using MoneyTrackerProject.Models;
 using System;
+using System.Data;
+using System.Data.Entity;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.AspNet.Identity;
 
 namespace MoneyTrackerProject.Controllers
 {
     [Authorize]
     public class SendEmailsController : Controller
     {
+        private MoneyTrackerDBModelContainer db = new MoneyTrackerDBModelContainer();
         // GET: SendEmails
         public ActionResult Index()
         {
@@ -48,6 +53,71 @@ namespace MoneyTrackerProject.Controllers
             }
 
             return View();
+        }
+        // GET: SendBulkEmails
+        public ActionResult SendBulk()
+        {
+            return View(new SendBulkEmailsViewModel());
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [ValidateInput(false)]
+        public ActionResult SendBulk(SendBulkEmailsViewModel model)
+        {
+            var userManager = HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            var userId = User.Identity.GetUserId();
+            IEnumerable<String> roleId = userManager.FindById(userId).Roles.Select(r => r.RoleId);
+            List<string> EmployeeList = new List<string>();
+            try
+            {
+                int role_id = Convert.ToInt32(roleId.ElementAt(0));
+                //here 4 is the roleId for Administrator. Administrator can view all transactions.
+                if (role_id == 4 || role_id == 5)
+                {
+                    var employees = db.Employees.Include(d => d.Department);
+                    foreach (var i in employees)
+                    {
+                        EmployeeList.Add(i.EmployeeEmail);
+                    }
+                }
+                else if (roleId != null && roleId.GetEnumerator().MoveNext())
+                {
+                    var employees = db.Employees.Where(e => e.DeptId == role_id);
+                    foreach (var i in employees)
+                    {
+                        EmployeeList.Add(i.EmployeeEmail);
+                    }
+                }
+                else
+                {
+                    return View("~/Views/Home/Unauthorized.cshtml");
+                }
+                List<String> toEmail = EmployeeList;
+                if (ModelState.IsValid)
+                {
+                    try
+                    {
+                        String subject = model.Subject;
+                        String contents = model.Contents;
+                        SendBulkEmail es = new SendBulkEmail();
+                        es.Send(toEmail, subject, contents);
+
+                        ViewBag.Result = "Email has been sent.";
+                        ModelState.Clear();
+                        return View(new SendBulkEmailsViewModel());
+                    }
+                    catch
+                    {
+                        return View();
+                    }
+                }
+                return View();
+            }
+            catch
+            {
+                return View("~/Views/Home/Unauthorized.cshtml");
+            }
         }
     }
 }
---
+++ MoneyTrackerProject/Utilities/SendBulkEmail.cs
@@ -0,0 +1,31 @@
+﻿using SendGrid;
+using SendGrid.Helpers.Mail;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace MoneyTrackerProject.Utilities
+{
+    public class SendBulkEmail
+    {
+        // Please use your API KEY here.
+        private const String API_KEY = "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";
+
+        public void Send(List<String> toEmail, String subject, String contents)
+        {
+            var client = new SendGridClient(API_KEY);
+            var from = new EmailAddress("benvargheset@gmail.com", "MoneyTracker Mail Service");
+            var to = new List<EmailAddress>();
+            foreach (string email in toEmail)
+            {
+                to.Add(new EmailAddress(email));
+            }
+            var plainTextContent = contents;
+            var htmlContent = "<p>" + contents + "</p>";
+            var msg = MailHelper.CreateSingleEmailToMultipleRecipients(from, to, subject, plainTextContent, htmlContent);
+            var response = client.SendEmailAsync(msg);
+        }
+    }
+}
