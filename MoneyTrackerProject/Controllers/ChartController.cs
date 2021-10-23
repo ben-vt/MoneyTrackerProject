c9df08e772772fe452e2cac6cf6c93c0ad1f4eae
ben-vt <61205390+ben-vt@users.noreply.github.com>
2021-10-23 15:05:00 +0530

    Added charts to the application -Added C3 chart plugin for implementing charts -Added a pie chart for department wise funds -Added a bar chart for expense and profit of each department

---
+++ MoneyTrackerProject/Controllers/ChartController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Net;
+using MoneyTrackerProject.Models;
+
+namespace MoneyTrackerProject.Controllers
+{
+    public class ChartController : Controller
+    {
+        // GET: Chart
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult PieChart()
+        {
+            MoneyTrackerDBModelContainer db = new MoneyTrackerDBModelContainer();
+            bool proxyCreation = db.Configuration.ProxyCreationEnabled;
+            try
+            {
+                db.Configuration.ProxyCreationEnabled = false;
+                var data = db.Departments.ToList();
+                return Json(data, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                Response.StatusCode = (int)HttpStatusCode.BadRequest;
+                return Json(ex.Message);
+            }
+            finally
+            {
+                db.Configuration.ProxyCreationEnabled = proxyCreation;
+            }
+        }
+
+        public ActionResult BarChart()
+        {
+            MoneyTrackerDBModelContainer db = new MoneyTrackerDBModelContainer();
+            var data = db.Database.SqlQuery<TotalPE>("exec TotalPE").ToList();
+            return Json(data, JsonRequestBehavior.AllowGet);
+        }
+
+        public class TotalPE
+        {
+            public string DEPTNAME { get; set; }
+            public Nullable<decimal> EXPENSE { get; set; }
+            public Nullable<decimal> PROFIT { get; set; }
+
+        }
+    }
+}
---
+++ TotalPE.sql
@@ -0,0 +1,18 @@
+﻿CREATE PROCEDURE TotalPE
+AS
+BEGIN
+SELECT dept.DepartmentName as DEPTNAME, 
+		SUM(CASE
+			WHEN txn.FKTransModeId = 1 THEN txn.ExpenseAmount
+		END) as EXPENSE,
+		SUM(CASE
+			WHEN txn.FKTransModeId = 2 THEN txn.ExpenseAmount
+		END) as PROFIT
+FROM Transactions txn join 
+	Departments dept
+	on dept.DepartmentId = txn.FKDeptId
+	group by dept.DepartmentName
+END
+GO
+
+EXEC TotalPE
